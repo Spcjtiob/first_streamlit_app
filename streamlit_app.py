0cebe2f76bf0f5ca1c92134d3b886527a450b607
Spcjtiob <108783423+Spcjtiob@users.noreply.github.com>
2022-07-06 18:35:35 +1200

    Update streamlit_app.py
    Adding another header

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,6 @@ streamlit.text("🥗 Blueberry Smoothie with Oat Milk")
 streamlit.text("🥑 Avocado with Tofu Scramble")
 streamlit.text("🥣 Chocolate protein Oatmeal")
 streamlit.text("🍞 Bagel with Hummus")
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
