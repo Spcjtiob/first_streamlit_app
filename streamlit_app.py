fe52ed53c87c9c18d2c8ec8b9221be351e459ea3
Spcjtiob <108783423+Spcjtiob@users.noreply.github.com>
2022-07-06 19:01:09 +1200

    Update streamlit_app.py
    Setting Fruit name as an index

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,7 @@ streamlit.text("🍞 Bagel with Hummus")
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #Let's put a fruit pick list here so the user can pick their own fruits
 streamlit.multiselect("Pick your fruits for your smoothie!", list(my_fruit_list.index))
