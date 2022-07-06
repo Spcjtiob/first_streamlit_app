5574b9b778602cf9f9de6618a924b441084b7fd0
Spcjtiob <108783423+Spcjtiob@users.noreply.github.com>
2022-07-06 18:28:45 +1200

    Update streamlit_app.py
    Added Breakfast menu as a Header and menu options as texts

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,9 @@
 import streamlit    
 
 streamlit.title("My Parent's New Healthy Diner") 
+
+streamlit.header("Breakfast Menu")
+streamlit.text("Blueberry Smoothie with Oat Milk")
+streamlit.text("Avocado with Tofu Scramble")
+streamlit.text("Chocolate protein Oatmeal")
+streamlit.text("Bagel with Hummus")
