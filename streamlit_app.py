770d76ff88d6dcf448fd6226ddd3ac86608feee9
Spcjtiob <108783423+Spcjtiob@users.noreply.github.com>
2022-07-06 19:15:44 +1200

    Update streamlit_app.py
    Trying to fix the error my using location pointer my_fruit_list.loc[picked_fruits]

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #Let's put a fruit pick list here so the user can pick their own fruits
 picked_fruits = streamlit.multiselect("Pick your fruits for your smoothie!", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[picked_fruits]
 
 #Display the table on the page
-streamlit.dataframe(my_fruit_list.picked_fruits)
+streamlit.dataframe(fruits_to_show)
